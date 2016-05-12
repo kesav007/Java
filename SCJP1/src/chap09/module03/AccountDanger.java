22d5a46a43d0e73e0340da4e03f3779d34fd4dc7
Kesav Nallan <kesav.nallan@gmail.com>
2016-05-12 19:03:42 -0400

    Test

--- SCJP1/src/chap09/module03/AccountDanger.java
+++ SCJP1/src/chap09/module03/AccountDanger.java
@@ -5,9 +5,13 @@ public class AccountDanger implements Runnable {
 	Account acct = new Account();
 
 	public static void main(String[] args) {
+
+		System.out.println("==============================================");
+		System.out.println("Lucy and Fred has common account with $50 balance");
+		System.out.println("Each tries to withdraw $10 five times. See how it works");
+		System.out.println("==============================================");
+
 		AccountDanger r = new AccountDanger();
-		AccountDanger r2 = new AccountDanger();
-		
 		Thread one = new Thread(r);
 		Thread two = new Thread(r);
 		one.setName("Fred");
@@ -26,25 +30,22 @@ public class AccountDanger implements Runnable {
 		}
 	}
 
-	private synchronized void makeWithdrawal(int amt) {
+	private void makeWithdrawal(int amt) {
 		int balance = acct.getBalance();
 		if (balance >= amt) {
-			System.out.println(Thread.currentThread().getName()
-					+ " is going to withdraw");
+			System.out.println(Thread.currentThread().getName() + " is going to withdraw");
 
 			try {
 				Thread.sleep(500);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-			
+
 			acct.withdraw(amt);
-			System.out.println(Thread.currentThread().getName()
-					+ " completes the withdrawal");
+			System.out.println(Thread.currentThread().getName() + " completes the withdrawal");
 		} else {
-			System.out.println("Not enough in account for "
-					+ Thread.currentThread().getName() + " to withdraw "
-					+ balance);
+			System.out.println(
+					"Not enough in account for " + Thread.currentThread().getName() + " to withdraw " + balance);
 		}
 	}
 
